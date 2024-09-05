f9dc278ab7d4d29031860fba55bf6c9eed595889
Alex <alex.holder@bigfootds.com>
2024-09-05 20:55:04 +1000

    feat: encounter groups implemented

--- src/pokemonEncounter.js
+++ src/pokemonEncounter.js
@@ -101,3 +101,39 @@ encounterButton.addEventListener("click", async (event) => {
 });
 
 
+
+let encounterGroupButton = document.getElementById("pokemonGroupEncounter");
+
+encounterGroupButton.addEventListener("click", async () => {
+		// let pokemonResult1 = await getPokemon();
+		// renderPokemonData(pokemonResult1);
+		// let pokemonResult2 = await getPokemon();
+		// renderPokemonData(pokemonResult2);
+		// let pokemonResult3 = await getPokemon();
+		// renderPokemonData(pokemonResult3);
+		// let pokemonResult4 = await getPokemon();
+		// renderPokemonData(pokemonResult4);
+		// let pokemonResult5 = await getPokemon();
+		// renderPokemonData(pokemonResult5);
+		// let pokemonResult6 = await getPokemon();
+		// renderPokemonData(pokemonResult6);
+
+		let multiplePokemonResult = await Promise.all([
+			getPokemon(),
+			getPokemon(),
+			getPokemon(),
+			getPokemon(),
+			getPokemon(),
+			getPokemon(),
+		]);
+
+		console.log(multiplePokemonResult);
+
+		multiplePokemonResult.forEach(renderPokemonData);
+
+		// multiplePokemonResult.forEach((pokemonResult) => renderPokemonData(pokemonResult));
+
+		// multiplePokemonResult.forEach((pokemonResult) => {
+		// 	renderPokemonData(pokemonResult)
+		// });
+});
